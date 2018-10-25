ddaf1619c0ac186319524b3723a9ef272aed803d
Denny Nguyen <dennisdnguyen19@gmail.com>
2018-10-25 17:09:36 -0400

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String helloWorld = "What is there to life, but a never ending eternity of despair. A never ending eternity \n" +
+                            "of suffering. Life is nothing but a monotonous drone of repeated tortures. Day after day \n" +
+                            "we see the same faces, we see different faces, but all the same we see faces. A face. \n" +
+                            "A body. A life. A future. I see none of that. My eyes have been wrapped in darkness. \n" +
+                            "What more is there to say but...\n" +
+                            "\nHello world.";
+
+        System.out.println(helloWorld);
+    }
+}
